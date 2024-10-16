6541b120e446c93908fb3a8c96629a9a90ffd4c0
mhiu05 <minhhieuhh2k5@gmail.com>
2024-10-16 14:18:16 +0700

    update

---
+++ C++/Code PTIT/CPP0720_Day_so_Bitonic.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void faster()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+using ll = long long;
+
+const int mod = 1e9 + 7;
+const int MAXN = 1e5 + 5;
+
+int main()
+{
+    faster();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >>
+    }
+    return 0;
+}
--- C++/Pracetice_with_Kien_Senpei/Pr3.cpp
+++ C++/Pracetice_with_Kien_Senpei/Pr3.cpp
@@ -1,68 +1,47 @@
-// Bài toán phân tích số
-// Ví dụ N = 100, X = {10, 20, 50}
-// ta có 10 cách chia như sau:
-// 10 10 10 10 10 10 10 10 10 10
-// 10 10 10 10 10 10 10 10 20
-// 10 10 10 10 10 10 20 20
-// 10 10 10 10 10 50
-// 10 10 10 10 20 20 20
-// 10 10 10 20 50
-// 10 10 20 20 20 20
-// 10 20 20 50
-// 20 20 20 20 20
-// 50 50
-
 #include <bits/stdc++.h>
-#define fi first
-#define se second
-#define ms(a, n) memset(a, n, sizeof(a))
-#define endl "\n"
-#define PI 3.141592653589793238
 using namespace std;
-
-using ll = long long;
-
-typedef pair<ll, ll> pl;
-typedef vector<pl> vll;
-typedef pair<ll, int> pi;
-typedef vector<pi> vii;
-
-const ll modi = 1000000007;
-const int MAXN = 1e7 + 1;
 
 void faster()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    cout.tie(0);
 }
 
-int n = 100;
-int a[50], X[4] = {0, 10, 20, 50};
-int sum = 0, cnt = 0;
+using ll = long long;
 
-vector<int> v[100];
-void backtrack(int pos)
+const int mod = 1e9 + 7;
+
+struct Sv
 {
-    if (sum >= n)
+    string name, msv;
+    double gpa;
+};
+
+void nhap(Sv a[], int n)
+{
+    for (int i = 0; i < n; ++i)
     {
-        if (sum == n)
-        {
-            for (int i = 1; i <= pos - 1; ++i)
-            {
-                v[cnt].push_back(a[i]);
-            }
-            ++cnt;
-        }
-        return;
+        cin.ignore();
+        getline(cin, a[i].name);
+        getline(cin, a[i].msv);
+        cin >> a[i].gpa;
     }
-    for (int i = 1; i <= 3; ++i)
+}
+
+bool cmp(Sv a, Sv b)
+{
+    return a.gpa < b.gpa;
+}
+
+void in(Sv a[], int n)
+{
+    for (int i = 0; i < n; ++i)
     {
-        a[pos] = X[i];
-        if (a[pos] >= a[pos - 1])
+        if (a[i].gpa >= 7.0)
         {
-            sum += X[i];
-            backtrack(pos + 1);
-            sum -= X[i];
+            cout << a[i].name << " " << a[i].msv << " ";
+            cout << fixed << setprecision(1) << a[i].gpa << endl;
         }
     }
 }
@@ -70,29 +49,11 @@ void backtrack(int pos)
 int main()
 {
     faster();
-    backtrack(1);
-    for (int i = 0; i < v[0].size(); ++i)
-    {
-        cout << v[9][i] << " ";
-    }
-    int min_val = 1000000000;
-    for (int i = 0; i < 100; ++i)
-    {
-        if (v[i].size() < min_val)
-        {
-            min_val = v[i].size();
-        }
-    }
-    for (int i = 0; i < 100; ++i)
-    {
-        if (min_val == v[i].size())
-        {
-            for (int j = 0; j < v[i].size(); ++j)
-            {
-                cout << v[i][j] << " ";
-            }
-            cout << endl;
-        }
-    }
+    int n;
+    cin >> n;
+    Sv a[100];
+    nhap(a, n);
+    sort(a, a + n, cmp);
+    in(a, n);
     return 0;
 }
