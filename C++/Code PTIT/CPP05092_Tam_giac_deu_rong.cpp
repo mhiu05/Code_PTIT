b602239cc27693502f0b4176db4f02c3cb0c98a1
mhiu05 <minhhieuhh2k5@gmail.com>
2024-11-13 21:33:20 +0700

    update

--- C++/Code PTIT/CPP05092_Tam_giac_deu_rong.cpp
+++ C++/Code PTIT/CPP05092_Tam_giac_deu_rong.cpp
@@ -12,25 +12,9 @@ using ll = long long;
 
 const int mod = 1e9 + 7;
 
-struct SinhVien
-{
-public:
-    int age;
-    void setAge(int age)
-    {
-        this->age = age;
-    }
-    // SinhVien() {};
-    SinhVien(int age)
-    {
-        age = age;
-    }
-};
-
 int main()
 {
     faster();
-    SinhVien sv;
-    cout << sv.age << endl;
+
     return 0;
 }
